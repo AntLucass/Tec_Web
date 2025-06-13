dda3ddd21119fb19be0699a6dc1ef141900b12a3
AntLucass <antoniolucasfreire17@gmail.com>
2025-06-12 23:54:26 -0300

    Pratica 5 adicionada

---
+++ Pratica_5/main.js
@@ -0,0 +1,16 @@
+const botao = document.getElementById("botao");
+const clicks = document.getElementById("clicks");
+cont = 0;
+
+botao.addEventListener("click", Contador);
+
+function Contador(){
+    cont++;
+    clicks.innerText = `Número de clicks: ${cont}`;
+
+    if(cont%1000==0){
+        clicks.innerText = `Número de clicks: ${cont} \n Parabéns pela insistência`;
+    }
+
+
+}
